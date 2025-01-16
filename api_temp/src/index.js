201da2ccdfe2bc70cca7f1a5582f4fe7ddf9e5b8
Chantal <chantiicou@gmail.com>
2025-01-16 10:59:52 -0300

    node/start

--- api_temp/src/index.js
+++ api_temp/src/index.js
@@ -1,3 +1,63 @@
+// require("dotenv").config();
+// const cors = require("cors");
+// const { json } = require("express");
+// const authRoutes = require("./routes/authRoutes");
+// const pool = require("./config/db");
+// const sequelize = require("./config/db");
+// const bodyParser = require("body-parser");
+// const mailerRoutes = require("./routes/nodeMailerRoutes");
+
+// if (
+//   !process.env.DB_USER ||
+//   !process.env.DB_NAME ||
+//   !process.env.DB_PASSWORD ||
+//   !process.env.PORT
+// ) {
+//   throw new Error("Faltan variables de entorno críticas");
+// }
+
+// const express = require("express");
+
+// const app = express();
+// const PORT = process.env.PORT || 5000;
+
+// app.use(
+//   cors({
+//     origin: "http://localhost:3000",
+//     methods: ["GET", "POST", "PUT", "DELETE"],
+//     credentials: true,
+//   })
+// );
+
+// app.use(json()); // Para manejar JSON sin usar bodyParser
+// app.use(bodyParser.json());
+// // Rutas
+// app.use("/", authRoutes);
+// app.use("/send", mailerRoutes);
+
+// // Middleware global para manejo de errores
+// app.use((err, req, res, next) => {
+//   console.error("Error:", err.message);
+//   res
+//     .status(500)
+//     .json({ error: "Ha ocurrido un error interno en el servidor" });
+// });
+
+// // sequielize conexion
+// sequelize
+//   .query("SELECT NOW()")
+//   .then(([results, metadata]) => {
+//     console.log("Conexión a la base de datos PostgreSQL exitosa");
+//   })
+//   .catch((err) => {
+//     console.error("Error al conectarse a la base de datos:", err);
+//   });
+// // Iniciar el servidor
+// app.listen(PORT, () => {
+//   console.log(`Servidor corriendo en el puerto ${PORT}`);
+// });
+
+// sequelize.sync({ force: false });
 require("dotenv").config();
 const cors = require("cors");
 const { json } = require("express");
@@ -7,11 +67,13 @@ const sequelize = require("./config/db");
 const bodyParser = require("body-parser");
 const mailerRoutes = require("./routes/nodeMailerRoutes");
 
+// Verifica si faltan variables de entorno críticas
 if (
   !process.env.DB_USER ||
   !process.env.DB_NAME ||
   !process.env.DB_PASSWORD ||
-  !process.env.PORT
+  !process.env.PORT ||
+  !process.env.DATABASE_URL
 ) {
   throw new Error("Faltan variables de entorno críticas");
 }
@@ -21,6 +83,7 @@ const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+// Configuración de CORS
 app.use(
   cors({
     origin: "http://localhost:3000",
@@ -31,6 +94,7 @@ app.use(
 
 app.use(json()); // Para manejar JSON sin usar bodyParser
 app.use(bodyParser.json());
+
 // Rutas
 app.use("/", authRoutes);
 app.use("/send", mailerRoutes);
@@ -43,18 +107,20 @@ app.use((err, req, res, next) => {
     .json({ error: "Ha ocurrido un error interno en el servidor" });
 });
 
-// sequielize conexion
+// Conexión a la base de datos con Sequelize (usando DATABASE_URL)
 sequelize
-  .query("SELECT NOW()")
-  .then(([results, metadata]) => {
+  .authenticate()
+  .then(() => {
     console.log("Conexión a la base de datos PostgreSQL exitosa");
   })
   .catch((err) => {
     console.error("Error al conectarse a la base de datos:", err);
   });
+
 // Iniciar el servidor
 app.listen(PORT, () => {
   console.log(`Servidor corriendo en el puerto ${PORT}`);
 });
 
+// Sincronizar Sequelize
 sequelize.sync({ force: false });
