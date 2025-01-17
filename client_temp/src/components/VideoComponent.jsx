481a0b6f5fe03c6ecbab528b0bc1fa14ce3b72ba
Chantal <chantiicou@gmail.com>
2025-01-17 10:04:19 -0300

    optimizando tamaño  video-6-componente separado

--- client_temp/src/components/VideoComponent.jsx
+++ client_temp/src/components/VideoComponent.jsx
@@ -22,7 +22,7 @@ const VideoComponent = () => {
         style={{ width: "100%", height: "auto" }}
       >
         <source
-          src="https://res.cloudinary.com/dqgjcfosx/video/upload/w_480,q_auto,f_auto/v1234567/7667357-uhd_3840_2160_30fps_nm24my.mp4"
+          src="https://res.cloudinary.com/dqgjcfosx/video/upload/w_360,q_50,f_auto/v1234567/7667357-uhd_3840_2160_30fps_nm24my.mp4"
           type="video/mp4"
         />
         Your browser does not support the video tag.
