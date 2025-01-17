e372027ca19b823a081b9e177377c4ccf68acc50
Chantal <chantiicou@gmail.com>
2025-01-17 10:37:31 -0300

    optimizando tamaño  video-10-version mobile con imagen

--- client_temp/src/components/VideoComponent.jsx
+++ client_temp/src/components/VideoComponent.jsx
@@ -4,19 +4,25 @@ import ReactPlayer from "react-player";
 const VideoComponent = () => {
   const isMobile = window.innerWidth <= 768;
 
-  const videoUrl = isMobile
-    ? "https://res.cloudinary.com/dqgjcfosx/video/upload/w_360,q_50,f_auto/v1234567/7667357-uhd_3840_2160_30fps_nm24my.mp4"
-    : "https://res.cloudinary.com/dqgjcfosx/video/upload/w_720,q_auto,f_auto/v1234567/7667357-uhd_3840_2160_30fps_nm24my.mp4";
+  const videoUrl = "https://res.cloudinary.com/dqgjcfosx/video/upload/w_720,q_auto,f_auto/v1234567/7667357-uhd_3840_2160_30fps_nm24my.mp4";
+  const imageUrl = "https://res.cloudinary.com/dqgjcfosx/image/upload/v1726319618/pexels-alesiakozik-8327011_en2sjo.jpg"; // URL de la imagen estática
 
   return (
-    <ReactPlayer
-      url={videoUrl} // URL dinámica según el ancho del dispositivo
-      playing={false} // No se reproduce automáticamente
-      controls={true} // Mostrar controles
-      width="100%" // Ancho completo
-      height="auto" // Altura automática
-      muted={true} // Silenciar para permitir reproducción automática en mobile si necesario
-    />
+    <div className="video-container">
+      {/* En dispositivos móviles, mostramos una imagen estática */}
+      {isMobile ? (
+        <img src={imageUrl} alt="Imagen estática" width="100%" />
+      ) : (
+        <ReactPlayer
+          url={videoUrl} // Video URL para escritorio
+          playing={false} // No reproducir automáticamente
+          controls={true} // Mostrar controles
+          width="100%" // Ancho completo
+          height="auto" // Altura automática
+          muted={true} // Silenciar para permitir reproducción automática en mobile si necesario
+        />
+      )}
+    </div>
   );
 };
 
