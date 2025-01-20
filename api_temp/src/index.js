abf2bf998292b26886a2d4b5861cc5c019109eb1
Chantal <chantiicou@gmail.com>
2025-01-20 11:26:57 -0300

    cambiando las url para desarrollo-2

--- api_temp/src/index.js
+++ api_temp/src/index.js
@@ -1,64 +1,3 @@
-// require("dotenv").config();
-// const cors = require("cors");
-// const { json } = require("express");
-// const authRoutes = require("./routes/authRoutes");
-// const pool = require("./config/db");
-// const sequelize = require("./config/db");
-// const bodyParser = require("body-parser");
-// const mailerRoutes = require("./routes/nodeMailerRoutes");
-
-// if (
-//   !process.env.DB_USER ||
-//   !process.env.DB_NAME ||
-//   !process.env.DB_PASSWORD ||
-//   !process.env.PORT
-// ) {
-//   throw new Error("Faltan variables de entorno críticas");
-// }
-
-// const express = require("express");
-
-// const app = express();
-// const PORT = process.env.PORT || 5000;
-
-// app.use(
-//   cors({
-//     origin: "http://localhost:3000",
-//     methods: ["GET", "POST", "PUT", "DELETE"],
-//     credentials: true,
-//   })
-// );
-
-// app.use(json()); // Para manejar JSON sin usar bodyParser
-// app.use(bodyParser.json());
-// // Rutas
-// app.use("/", authRoutes);
-// app.use("/send", mailerRoutes);
-
-// // Middleware global para manejo de errores
-// app.use((err, req, res, next) => {
-//   console.error("Error:", err.message);
-//   res
-//     .status(500)
-//     .json({ error: "Ha ocurrido un error interno en el servidor" });
-// });
-
-// // sequielize conexion
-// sequelize
-//   .query("SELECT NOW()")
-//   .then(([results, metadata]) => {
-//     console.log("Conexión a la base de datos PostgreSQL exitosa");
-//   })
-//   .catch((err) => {
-//     console.error("Error al conectarse a la base de datos:", err);
-//   });
-// // Iniciar el servidor
-// app.listen(PORT, () => {
-//   console.log(`Servidor corriendo en el puerto ${PORT}`);
-// });
-
-// sequelize.sync({ force: false });
-
 require("dotenv").config();
 const cors = require("cors");
 const { json } = require("express");
@@ -96,12 +35,12 @@ const PORT = process.env.PORT || 5000;
 
 // Configurar los orígenes permitidos - DESARROLLO
 const corsOptions = {
-  origin: "https://apidc-bf-2.onrender.com/",
-
-  methods: ["GET", "HEAD", "PUT", "PATCH", "POST", "DELETE"],
+  origin: "https://apidc-bf-2.onrender.com",
+  methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
   credentials: true,
 };
 app.use(cors(corsOptions));
+
 // app.options("*", cors(corsOptions));
 
 app.use(json()); // Para manejar JSON sin usar bodyParser
--- client_temp/src/redux/actions/index.js
+++ client_temp/src/redux/actions/index.js
@@ -1,154 +1,11 @@
-// import { REGISTER_USER, SET_ERROR, SUBMIT_FORM } from "../actions_types";
-// import axios from "axios";
-
-// // Acción para registrar al usuario usando Axios
-// export const registerUser = (userData, token) => {
-//   return async (dispatch) => {
-//     try {
-//       // Petición POST al endpoint de registro
-//       const response = await axios.post(
-//         "http://localhost:5001/register",
-//         {
-//           email: userData.email,
-//           name: userData.name,
-//         },
-//         {
-//           headers: {
-//             "Content-Type": "application/json",
-//             Authorization: `Bearer ${token}`,
-//           },
-//         }
-//       );
-
-//       const { user } = response.data;
-
-//       console.log(response.data, "esto me deberia devovler todos los users");
-//       // Despachamos la acción de Redux con los datos del usuario
-//       dispatch({
-//         type: REGISTER_USER,
-//         payload: response.data.userAmin.isAdmin,
-//         allUsers: response.data.users,
-//       });
-
-//       // Guardamos si el usuario es admin en el localStorage
-//       localStorage.setItem("isAdmin", user.isAdmin);
-
-//       // Aquí se puede manejar cualquier lógica adicional, como redirigir al usuario
-//     } catch (error) {
-//       console.error("Error registrando usuario:", error);
-//       dispatch({
-//         type: SET_ERROR,
-//         payload: error.message,
-//       });
-//     }
-//   };
-// };
-
-// // export const registerUser = (userData, token) => {
-// //   return async (dispatch) => {
-// //     try {
-
-// //       const response = await axios.post(
-// //         "http://localhost:5001/register",
-// //         {
-// //           email: userData.email,
-// //           name: userData.name,
-// //         },
-// //         {
-// //           headers: {
-// //             "Content-Type": "application/json",
-// //             Authorization: `Bearer ${token}`,
-// //           },
-// //         }
-// //       );
-
-// //       const { user } = response.data;
-
-// //       dispatch({
-// //         type: REGISTER_USER,
-// //         payload: user,
-// //       });
-
-// //       localStorage.setItem("isAdmin", user.isAdmin);
-
-// //     } catch (error) {
-// //       console.error("Error registrando usuario:", error);
-// //       dispatch({
-// //         type: SET_ERROR,
-// //         payload: error.message,
-// //       });
-// //     }
-// //   };
-// // };
-
-// // Acción para enviar los datos del formulario al backend
-// export const formInfo = (formData) => async (dispatch) => {
-//   try {
-//     console.log(formData, "AKA ESTA LA DATA QUE VOY A ENVIAR");
-//     // Enviar los datos al backend
-//     const response = await axios.post(
-//       "http://localhost:5001/send/admin",
-//       formData,
-//       {
-//         headers: {
-//           "Content-Type": "application/json", // Si envías un archivo también
-//         },
-//       }
-//     );
-
-//     // Respuesta exitosa
-//     dispatch({
-//       type: "FORM_INFO_SUCCESS",
-//       payload: response.data, // Lo que devuelva el backend
-//     });
-//     console.log(response.data, "RESPUESTA DE MI BACKEND");
-//   } catch (error) {
-//     // Manejo de errores
-//     dispatch({
-//       type: "FORM_INFO_FAILURE",
-//       payload: error.response ? error.response.data : "Error en la solicitud",
-//     });
-//   }
-// };
-
-// export const fetchUsers = (token) => {
-//   return async (dispatch) => {
-//     try {
-//       console.log("Token enviado en fetchUsers:", token); // Verifica el token aquí
-//       const response = await axios.get("http://localhost:5001/users", {
-//         headers: {
-//           Authorization: `Bearer ${token}`, // Token de autenticación
-//         },
-//       });
-
-//       dispatch({
-//         type: "FETCH_USERS_SUCCESS",
-//         payload: response.data,
-//       });
-//     } catch (error) {
-//       console.error("Error obteniendo usuarios:", error);
-//       dispatch({
-//         type: SET_ERROR,
-//         payload: error.message,
-//       });
-//     }
-//   };
-// };
-
 import { REGISTER_USER, SET_ERROR, SUBMIT_FORM } from "../actions_types";
 import axios from "axios";
-
-// Dirección base de tu API en producción
-// const API_URL = "https://apidc-bf-2.onrender.com";
-const API_URL = process.env.REACT_APP_API_URL_DESARROLLO;
-console.log(API_URL, "APIURL");
 
 // Acción para registrar al usuario usando Axios
 export const registerUser = (userData, token) => {
   return async (dispatch) => {
     try {
       // Petición POST al endpoint de registro
-      // `${API_URL}/register`,
       const response = await axios.post(
         "https://apidc-bf-2.onrender.com/register",
         {
@@ -165,7 +22,6 @@ export const registerUser = (userData, token) => {
       );
 
       const { user } = response.data;
-      console.log(user, "USER EN EL ACTIONS");
       // Despachamos la acción de Redux con los datos del usuario
       dispatch({
         type: REGISTER_USER,
@@ -193,7 +49,7 @@ export const formInfo = (formData) => async (dispatch) => {
     // Enviar los datos al backend
     const response = await axios.post(
       "https://apidc-bf-2.onrender.com/send/admin",
-      // `${API_URL}/send/admin`,
+
       formData,
       {
         headers: {
@@ -222,12 +78,15 @@ export const fetchUsers = (token) => {
   return async (dispatch) => {
     try {
       console.log("Token enviado en fetchUsers:", token); // Verifica el token aquí
-      const response = await axios.get(`${API_URL}/users`, {
-        // Actualiza la URL
-        headers: {
-          Authorization: `Bearer ${token}`, // Token de autenticación
-        },
-      });
+      const response = await axios.get(
+        `https://apidc-bf-2.onrender.com/users`,
+        {
+          // Actualiza la URL
+          headers: {
+            Authorization: `Bearer ${token}`, // Token de autenticación
+          },
+        }
+      );
 
       dispatch({
         type: "FETCH_USERS_SUCCESS",
