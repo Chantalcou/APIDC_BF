bb5f85c961e033f9a923da1c8575e2e750b2feeb
Chantal <chantiicou@gmail.com>
2025-02-21 12:52:41 -0300

    cambiando config.db

--- api_temp/src/config/db.js
+++ api_temp/src/config/db.js
@@ -1,20 +1,43 @@
-// config/database.js
+// // config/database.js
+// const { Sequelize } = require("sequelize");
+// require('dotenv').config();
+
+// console.log("DB_PASSWORD:", process.env.DB_PASSWORD);
+
+// const sequelize = new Sequelize(
+//   process.env.DB_NAME,
+//   process.env.DB_USER,
+//   process.env.DB_PASSWORD,
+//   {
+//     host: process.env.DB_HOST,
+//     port: process.env.DB_PORT,
+//     dialect: "postgres",
+//     logging: false,
+//   }
+// );
+
+// // Probar la conexión
+// async function testConnection() {
+//   try {
+//     await sequelize.authenticate();
+//     console.log("Conexión a la base de datos establecida correctamente.");
+//   } catch (error) {
+//     console.error("No se pudo conectar a la base de datos:", error);
+//   }
+// }
+
+// testConnection();
+
+// module.exports = sequelize;
+
 const { Sequelize } = require("sequelize");
-require('dotenv').config(); 
+require("dotenv").config();
 
-console.log("DB_PASSWORD:", process.env.DB_PASSWORD);
-
-const sequelize = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USER,
-  process.env.DB_PASSWORD,
-  {
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    dialect: "postgres",
-    logging: false,
-  }
-);
+// Usar DATABASE_URL de las variables de entorno
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
+  dialect: "postgres",
+  logging: false,
+});
 
 // Probar la conexión
 async function testConnection() {
